6843aabed1b94cbe6fb07ad725d4b8823c966edd
Elijah Cruz <ecwebservices@gmail.com>
2021-07-26 00:13:55 -0500

    fixed typo.

--- src/AvatarServiceProvider.php
+++ src/AvatarServiceProvider.php
@@ -10,7 +10,7 @@ class AvatarServiceProvider extends ServiceProvider
     {
         $this->app->bind('avatar', function($app) {
             return new AvatarLaravel();
-        })
+        });
     }
 
     public function boot()
