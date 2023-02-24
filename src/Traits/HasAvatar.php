b174e64b9a37d59823b438f25ba4c4df83ed8f3b
Elijah Cruz <ecwebservices@gmail.com>
2023-02-23 16:26:23 -0800

    Updated trait to be simpler

--- src/Traits/HasAvatar.php
+++ src/Traits/HasAvatar.php
@@ -3,6 +3,8 @@
 namespace Elijahcruz\Avatar\Traits;
 
 use Elijahcruz\Avatar\AvatarLaravel;
+use Elijahcruz\Avatar\Exception\GeneratorTypeNotFound;
+use Elijahcruz\Avatar\Exception\IdentifierTypeNotSupportedException;
 
 /**
  * Trait HasAvatar
@@ -10,76 +12,20 @@ use Elijahcruz\Avatar\AvatarLaravel;
  */
 trait HasAvatar
 {
-    protected $avatarIdentifier = 'email';
-    protected $avatarProvider = 'gravatar';
-    protected $avatarOptions = [];
+    protected string $avatarIdentifier;
+    protected string $avatarProvider;
+    protected array $avatarOptions;
 
-    public function getAvatarAttribute()
+    /**
+     * Get the URL
+     *
+     * @return string
+     * @throws GeneratorTypeNotFound
+     * @throws IdentifierTypeNotSupportedException
+     */
+    protected function getAvatar(): string
     {
-        return $this->getAvatar();
-    }
-
-    public function getAvatar($size = 100)
-    {
-        // Check if the identifier is an actual string and not null
-        if (is_string($this->{$this->avatarIdentifier})) {
-            $this->options['size'] = $size;
-        }
-
-
-        $avatar = new AvatarLaravel($this->{$this->avatarIdentifier}, $this->avatarProvider, $this->avatarOptions);
-
+        $avatar = new AvatarLaravel($this->avatarIdentifier, $this->avatarProvider, $this->avatarOptions);
         return $avatar->getUrl();
-    }
-
-    /**
-     * @return string
-     */
-    public function getAvatarIdentifier(): string
-    {
-        return $this->avatarIdentifier;
-    }
-
-    /**
-     * @param string $avatarIdentifier
-     */
-    public function setAvatarIdentifier(string $avatarIdentifier): void
-    {
-        $this->avatarIdentifier = $avatarIdentifier;
-    }
-
-    /**
-     * @return string
-     */
-    public function getAvatarProvider(): string
-    {
-        return $this->avatarProvider;
-    }
-
-    /**
-     * @param string $avatarProvider
-     */
-    public function setAvatarProvider(string $avatarProvider): void
-    {
-        $this->avatarProvider = $avatarProvider;
-    }
-
-    /**
-     * @return array
-     */
-    public function getAvatarOptions(): array
-    {
-        return $this->avatarOptions;
-    }
-
-    /**
-     * @param array $avatarOptions
-     */
-    public function setAvatarOptions(array $avatarOptions): void
-    {
-        try{
-            $this->setAvatarInfo();
-        }
-        $this->avatarOptions = $avatarOptions;
     }
 }
